c7be65d5c7194a9f0b374633abee25ef31cf6136
RubySometimes <alias@marchy.me>
2017-09-25 21:00:07 -0400

    Removed shop.json Removed the shop.json file and the data/ directory.  Instead it is linked via my website.

--- src/osrs.rb
+++ src/osrs.rb
@@ -5,5 +5,5 @@ module OSRS
   # Links to image data on the OSRS GE
   GE_IMAGE = 'http://services.runescape.com/m=itemdb_oldschool/1505990121692_obj_big.gif?id='.freeze
   # Link to Shop Data
-  SHOP_DATA = 'https://raw.githubusercontent.com/RubySometimes/alch.rb/master/data/shop.json'.freeze
+  SHOP_DATA = 'http://marchy.me/data/shop.json'.freeze
 end

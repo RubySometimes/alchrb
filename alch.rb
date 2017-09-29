23ace84e630ce7d5ee63527d04d253584409ba05
RubySometimes <alias@marchy.me>
2017-09-29 14:14:34 -0400

    Added version number Also added additional comments.

--- alch.rb
+++ alch.rb
@@ -1,4 +1,8 @@
 # Require this file if you are to use this in a project
+# Released under the MIT License agreement
+
+# @author RubySometimes (GitHub)
+# @version 1.0.5
 require_relative('src/item')
 require_relative('src/osrs')
 require_relative('src/runes')

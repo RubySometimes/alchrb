84d9bf0f5fdb9cb8c1523a00924a81d9db5a7f0f
marchsabino <msabino@cs.hartford.edu>
2017-10-16 19:34:33 -0400

    Updated version

--- lib/alch.rb
+++ lib/alch.rb
@@ -2,7 +2,7 @@
 # Released under the MIT License agreement
 
 # @author RubySometimes (GitHub)
-# @version 1.1.0
+# @version 1.1.1
 require_relative('alch/item')
 require_relative('alch/osrs')
 require_relative('alch/runes')
